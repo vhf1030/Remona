db89085feff06e22e1fc1e09985977b26bfa1a57
vhf1030 <vhf1030@naver.com>
2023-08-07 11:52:53 +0900

    add theme info table to front

--- app/views.py
+++ app/views.py
@@ -9,6 +9,22 @@ def metainfo_list(request):
     context = {'metainfos': metainfos}
     return render(request, 'theme_info.html', context)
 
+def get_meta_filter_dataset(request):
+    loc_2_choices = MetaInfo.objects.values_list('loc_2', flat=True).distinct()
+
+    loc_2_value = request.GET.get('loc_2_filter', None)
+    if loc_2_value:
+        filtered_data = MetaInfo.objects.filter(loc_2=loc_2_value)
+    else:
+        filtered_data = MetaInfo.objects.all()
+
+    context = {
+        'data': filtered_data,
+        'loc_2_choices': loc_2_choices,
+    }
+    return render(request, 'theme_info.html', context)
+
+
 def reserve_info(request):
     reserve_info_list = ReserveInfo.objects.all()
     return render(request, 'reserve_info.html', {'reserve_info_list': reserve_info_list})
